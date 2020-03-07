e3f21acea7467c9cd59de4381cc7ccf4d50403ca
“digu-007” <jasonryder555@gmail.com>
2020-03-07 21:55:32 +0530

    integration with web3 and metamask

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,20 +4,35 @@ contract Election {
     struct Candidate {
         uint id;
         string name;
-        uint votes;
+        uint voteCount;
     }
 
+    mapping(address => bool) public voters;
     mapping(uint => Candidate) public candidates;
 
     uint public cnt;
 
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
     constructor () public {
-        add("chirag");
-        add("tony");
+        addCandidate("chirag");
+        addCandidate("tony");
     }
 
-    function add(string memory _name) private {
+    function addCandidate(string memory _name) private {
         ++cnt;
         candidates[cnt] = Candidate(cnt, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "voting twice!");
+        require((_candidateId > 0 && _candidateId <= cnt), "invalid id!");
+
+        voters[msg.sender] = true;
+        ++candidates[_candidateId].voteCount;
+
+        emit votedEvent(_candidateId);
     }
 }
